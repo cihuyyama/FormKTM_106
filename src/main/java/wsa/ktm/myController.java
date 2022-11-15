51012a0fd9ae676ce08f35ae4a44fc4206dc3bef
cihuyyama <miqbalalhabib@gmail.com>
2022-11-15 22:18:14 +0700

    coding controller

--- src/main/java/wsa/ktm/myController.java
+++ src/main/java/wsa/ktm/myController.java
@@ -4,10 +4,48 @@
  */
 package wsa.ktm;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
 /**
  *
  * @author iqbal al habib
  */
 public class myController {
-    
+    @RequestMapping(value = "/getinto")
+    public String getForm(
+        @RequestParam(value = "name") String isinama,
+        @RequestParam(value = "nim") String isinim,
+        @RequestParam(value = "locate") String isitempat,
+        @RequestParam("date")
+        @DateTimeFormat (pattern="yyyy-MM-dd") Date date,
+        @RequestParam(value = "major") String isijrsn,
+        @RequestParam(value = "email") String isiemail,
+        @RequestParam(value = "img") MultipartFile img,
+            Model kurir
+    ) throws IOException{
+        SimpleDateFormat newTanggal = new SimpleDateFormat("dd-MM-yyyy");
+        
+        String tanggalku = newTanggal.format(date);
+        
+        String blob = Base64.encodeBase64String(img.getBytes());
+        String gmbr = "data:image/*;base64,".concat(blob);
+        
+        kurir.addAttribute("kname", isinama);
+        kurir.addAttribute("knim", isinim);
+        kurir.addAttribute("klocate", isitempat);
+        kurir.addAttribute("kdate", tanggalku);
+        kurir.addAttribute("kmajor", isijrsn);
+        kurir.addAttribute("kemail", isiemail);
+        kurir.addAttribute("kimg", gmbr);
+        
+        return "view";
+    }
 }
